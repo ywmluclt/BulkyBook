87ff85b972d09ac224c5d31e79c0c903f99c1096
ywmluclt <ywm.luclt@gmail.com>
2021-01-11 08:26:59 +0700

    add product

---
+++ BulkyBook.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,42 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Product product)
+        {
+            var productFromDb = _db.Products.FirstOrDefault(predicate => predicate.Id == product.Id);
+            if (productFromDb != null)
+            {
+                if (product.ImageUrl != null)
+                {
+                    productFromDb.ImageUrl = product.ImageUrl;
+                }
+                productFromDb.ISBN = product.ISBN;
+                productFromDb.ListPrice = product.ListPrice;
+                productFromDb.Price = product.Price;
+                productFromDb.Price100 = product.Price100;
+                productFromDb.Price50 = product.Price50;
+                productFromDb.Title = product.Title;
+                productFromDb.Description = product.Description;
+                productFromDb.CoverTypeId = product.CoverTypeId;
+                productFromDb.CategoryId = product.CoverTypeId;
+
+            }
+        }
+    }
+}
--- BulkyBook/Areas/Customer/Controllers/HomeController.cs
+++ BulkyBook/Areas/Customer/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using BulkyBook.Models.ViewModels;
+using BulkyBook.DataAccess.Repository.IRepository;
 
 namespace BulkyBook.Areas.Customer.Controllers
 {
@@ -13,15 +14,18 @@ namespace BulkyBook.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var allProducts = _unitOfWork.Product.GetAll(includeProperties: "Category,CoverType");
+            return View(allProducts);
         }
 
         public IActionResult Privacy()
